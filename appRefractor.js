a0af7d372d0ac30b2a89933a8a9b55cadfc2e408
kemiwins <kemi.mail@gmail.com>
2014-05-12 14:37:45 -0400

    refractor - created function

---
+++ appRefractor.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	$("#number_input").keyup(function(event) {
+		if(event.keyCode == 13) {
+			var fizzBuzz = function(integer) {
+			var clear = $("#number_input").val(" ");		
+				for (i=1; i<=integer && i<=100 ; i++)	{
+					if(i%3 == 0 && i%5 ==0) {
+						$("body").append("<p>" + "fizz buzz");
+					}
+					else if(i%3 == 0) {
+						$("body").append("<p>" + "fizz");
+					}
+					else if(i%5 == 0) {
+						$("body").append("<p>" + "buzz");
+					}
+					else{
+						$("body").append("<p>" + i);
+					}
+					$(clear).hide("#number_input");
+				}
+				if(isNaN(integer) || integer%1 !=0 || integer >= +"101") {
+					alert("Please enter a whole number from 1 - 100!");
+					$(clear);
+				}
+			}
+			fizzBuzz( +$("#number_input").val() );
+		}			
+	});
+	
+});
